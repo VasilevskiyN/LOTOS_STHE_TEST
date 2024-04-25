49052e9b6ca5d2ea620ff43ecbc7032aa939976f
Vasilevskiy <n_vasilevskiy@list.ru>
2024-04-25 13:56:17 +0500

    24.04.2024 12:15

--- TEST_API.py
+++ TEST_API.py
@@ -6,13 +6,20 @@ import time
 # 1355 // 59843251
 # 9548 // 91245684
 # 6432 // 72648953
-login = int(input('Введите login '))
-number = int(input('Введите number '))
-T1 = int(input('Введите температуру на входе горячего потока '))
-T2 = int(input('Введите температуру на выходе горячего потока '))
-t1 = int(input('Введите температуру на входе холодного потока '))
-t2 = int(input('Введите температуру на выходе холодного потока '))
-g = int(input('Введите расход холодной среды '))
+#login = int(input('Введите login '))
+#number = int(input('Введите number '))
+#T1 = int(input('Введите температуру на входе горячего потока '))
+#T2 = int(input('Введите температуру на выходе горячего потока '))
+#t1 = int(input('Введите температуру на входе холодного потока '))
+#t2 = int(input('Введите температуру на выходе холодного потока '))
+#g = int(input('Введите расход холодной среды '))
+login = 1877
+number = 28791562
+T1 = 70
+T2 = 55
+t1 = 10
+t2 = 35
+g = 5
 G = int(input('Введите расход горячей среды '))
 
 API_URL='http://localhost:8000/license'
@@ -31,4 +38,14 @@ print(result2, result2.text)
 
 API_URL='http://localhost:8000/dashboard'
 result3 = requests.get(API_URL)
-print(result3)
+task_id = result3.json()['task_id']
+print(result3.json())
+
+task_status = ''
+while "SUCCESS" not in task_status:
+    API_URL = f'http://localhost:8000/get_status?task_id={task_id}'
+    result4 = requests.get(API_URL)
+    time.sleep(5)
+    task_status = result4.text
+    print(task_status)
+print(f"Задача №{task_id} выполнена")
