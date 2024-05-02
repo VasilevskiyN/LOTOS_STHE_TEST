5153f8788a93a696f56d33acd6e4c295cd20ecdf
Vasilevskiy <n_vasilevskiy@list.ru>
2024-05-02 15:08:32 +0500

    02.05.2024 15:08

--- TEST_API.py
+++ TEST_API.py
@@ -2,7 +2,7 @@ import requests
 import time
 import random
 
-r= random.randint(0, 4)
+r = random.randint(0, 4)
 a = [1455, 1877, 1355, 9548, 6432]
 b = {
     '1455': 19052013,
@@ -28,30 +28,7 @@ G = random.randint(10, 50)
 #g = int(input('Введите расход холодной среды '))
 #G = int(input('Введите расход горячей среды '))
 
-API_URL='http://localhost:8000/license'
-data1 = {'login' : login, 'number' : number}
-result1 = requests.post(API_URL,params = data1)
-print(result1, result1.text)
-
-API_URL='http://localhost:8000/initial_data'
-data =  {'t1':t1,'t2':t2,'T1':T1, 'T2':T2, 'g':g, 'G':G}
-result = requests.post(API_URL,params = data)
-print(result, result.text)
-
-API_URL='http://localhost:8000/TEMA'
-result2 = requests.get(API_URL)
-print(result2, result2.text)
-
 API_URL='http://localhost:8000/dashboard'
-result3 = requests.get(API_URL)
-task_id = result3.json()['task_id']
-print(result3.json())
-
-task_status = ''
-while "SUCCESS" not in task_status:
-    API_URL = f'http://localhost:8000/get_status?task_id={task_id}'
-    result4 = requests.get(API_URL)
-    time.sleep(5)
-    task_status = result4.text
-    print(task_status)
-print(f"Задача № {task_id} выполнена")
+data = {'login': login, 'number': number, 't1': t1, 't2': t2, 'T1': T1, 'T2': T2, 'g': g, 'G': G}
+result = requests.post(API_URL,params = data)
+print(f"Задача № {result.text} получена")
